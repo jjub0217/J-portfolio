c9c3f3901ece5104300697496f10ff7593a22fd2
jjub0217 <noonssub1004@gmail.com>
2025-04-11 17:16:25 +0900

    이미지 최적화 시도 중

--- assets/js/main.js
+++ assets/js/main.js
@@ -231,6 +231,7 @@ const observer = new IntersectionObserver((entries) => {
     if (entry.isIntersecting) {
       const video = entry.target;
       video.src = video.dataset.src;
+      video.poster = video.dataset.poster; // 이 부분!
       observer.unobserve(video);
     }
   });
